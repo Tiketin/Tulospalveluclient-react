f51d87b4b69dcc41a802a978b704d9ed0f3e347c
Tiketin <nikke.tikka@hotmail.com>
2025-06-02 21:45:20 +0300

    numKeyboard

--- src/components/Molkky.js
+++ src/components/Molkky.js
@@ -293,7 +293,7 @@ const Molkky = () => {
             <Form.Label style={{fontWeight: "bold"}}>{gameInstruction}</Form.Label>
           </Col>
           <Col xs="auto">
-            <Form.Control type="text"
+            <Form.Control type="number"
                           value={newScore}
                           onChange={handleScoreChange}
                           placeholder="0-12"
